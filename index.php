cf8654ea760a9da00fec4e82ed84e7d9adf0eb2b
Sittichai <99570989+Mai-2541@users.noreply.github.com>
2022-09-19 13:30:52 +0700

    Index.php
    Index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php include "includes/header.php"; ?>
+
+  <head>
+
+  <body>
+    <?php include "includes/navbar.php"; ?>
+
+    <div class="container">
+      <!-- start -->
+      <h2>หน้าหลัก</h2>
+      <hr>
+      welcome
+    <!-- end  -->
+    </div>
+
+  </body>
+
+</html>
+<?php include "includes/footer.php"; ?>
